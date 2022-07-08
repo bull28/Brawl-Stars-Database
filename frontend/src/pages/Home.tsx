d54046cd409f0412aabea3e772bcea077c035958
Jason Louie <jason.jw.louie@gmail.com>
2022-07-07 17:39:12 -0700

    added backROUND

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,17 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Flex, Text, Image } from '@chakra-ui/react'
 
 
 export default function Home() {
   return (
-    <Flex w={'100%'} flexDir={'column'} textAlign={'center'}>
-        <Text fontSize={'3xl'}>
+    <>
+    <Image w={'100vw'} h={'100vh'} position={'absolute'} objectFit={'cover'} src={require('../assets/backround.webp')} zIndex={'-1'} fallbackSrc={require('../assets/backround.png')}/>
+      <Flex w={'100%'} flexDir={'column'} textAlign={'center'}>
+      
+        <Text fontSize={'3xl'} color={'red.500'} textShadow={'-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000'}>
             Brawl Stars Database
         </Text>
-    </Flex>
+        
+      </Flex>
+    </>
   )
 }
