4dc5b4efde8a601bab86e183d4ae1960922ac821
bull28 <koopagreenie28@gmail.com>
2022-08-02 22:54:20 -0700

    Collection page concept

--- backend/modules/pins.js
+++ backend/modules/pins.js
@@ -1,12 +1,20 @@
 function formatCollectionData(allSkins, userCollection, portraitFile, pinFile){
-    var collectionInfo = [];
+    var collectionInfo = {
+        "unlockedBrawlers":0,
+        "totalBrawlers":0,
+        "unlockedPins":0,
+        "totalPins":0,
+        "collection":[]
+    };
 
     // All other fields will not be included to minimize the data size
     const includeFromBrawler = ["name", "displayName", "portrait", "rarity", "pins"];
-
+    
     // Iterate over all the brawlers
     for (let brawler of allSkins){
         var thisBrawler = {};
+        var unlockedPins = 0;
+        var totalPins = 0;
 
         // Check that all desired properties exist all at once
         // so they do not have to be checked individually as
@@ -21,9 +29,13 @@ function formatCollectionData(allSkins, userCollection, portraitFile, pinFile){
         // This determines whether to check for unlocked pins
         // If a brawler is not unlocked, none of their pins can
         // be unlocked either.
-        let hasBrawler = false;
+        var hasBrawler = false;
         if (missingProperties == false){
             hasBrawler = userCollection.hasOwnProperty(brawler.name);
+            if (hasBrawler){
+                collectionInfo.unlockedBrawlers++;
+            }
+            collectionInfo.totalBrawlers++;
 
             var brawlerPins = [];
 
@@ -31,44 +43,45 @@ function formatCollectionData(allSkins, userCollection, portraitFile, pinFile){
             for (let pin of brawler.pins){
                 var thisPin = {};
 
-                // Iterate over the fields of a single pin object, do not include the rarity
-                for (let x in pin){
-                    if (x == "image"){
-                        thisPin["image"] = pin[x];
-                    }
+                if (pin.hasOwnProperty("image")){
+                    thisPin["i"] = pin["image"];
                 }
 
-                thisPin["unlocked"] = false;
+                thisPin["u"] = false;
 
                 // If the brawler appears in userCollection as a key, it is unlocked
                 // If the brawler is unlocked, check to see if the name of the current
                 // pin appears in the corresponding value. If it appears, the current
                 // pin is unlocked.
                 if (hasBrawler){    
-                    let hasPin = userCollection[brawler.name].includes(pin.name);
-                    if (hasPin == true){
-                        thisPin["unlocked"] = true;
+                    if (userCollection[brawler.name].includes(pin.name)){
+                        thisPin["u"] = true;
+                        unlockedPins++;
+                        collectionInfo.unlockedPins++;
                     }
                 }
+                totalPins++;
+                collectionInfo.totalPins++;
 
                 brawlerPins.push(thisPin);
             }
 
             thisBrawler["name"] = brawler.name;
             thisBrawler["displayName"] = brawler.displayName;
-            thisBrawler["portrait"] = portraitFile + brawler.portrait;
 
-            var rarityColor = "#000000";
+            thisBrawler["rarityColor"] = "#000000";
             if (brawler.rarity.hasOwnProperty("color")){
-                rarityColor = brawler.rarity.color;
+                thisBrawler["rarityColor"] = brawler.rarity.color;
             }
-
-            thisBrawler["rarityColor"] = rarityColor;
-            thisBrawler["unlocked"] = hasBrawler;
+            
+            thisBrawler["i"] = portraitFile + brawler.portrait;
+            thisBrawler["u"] = hasBrawler;
+            thisBrawler["unlockedPins"] = unlockedPins;
+            thisBrawler["totalPins"] = totalPins;
             thisBrawler["pinFilePath"] = pinFile + brawler.name + "/";
             thisBrawler["pins"] = brawlerPins;
 
-            collectionInfo.push(thisBrawler);
+            collectionInfo.collection.push(thisBrawler);
         }
     }
     return collectionInfo;
